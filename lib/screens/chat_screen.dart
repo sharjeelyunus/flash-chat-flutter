934d7ed02240f25647814d92490f1d0c2d258726
sharjeelyunus <sharjeel924@gmail.com>
2020-06-13 19:56:04 +0500

    👌 IMPROVE: Chat order

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -78,6 +78,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
+                        'timestamp': DateTime.now().millisecondsSinceEpoch,
                       });
                     },
                     child: Text(
@@ -99,7 +100,7 @@ class MessagesStream extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _firestore.collection('messages').snapshots(),
+      stream: _firestore.collection('messages').orderBy('timestamp').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Center(
